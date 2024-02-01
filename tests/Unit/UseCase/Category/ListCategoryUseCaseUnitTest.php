4a82ede9fa77d858d0111e039cbd6f95b9a3a0ef
Danrley Lima <danrleylac@gmail.com>
2024-02-01 17:59:49 -0300

    test: add unit test for ListCategoryUseCase

---
+++ tests/Unit/UseCase/Category/ListCategoryUseCaseUnitTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Unit\UseCase\Category;
+
+use App\Core\Domain\Repository\CategoryRepositoryInterface;
+use Core\Domain\Entity\Category;
+use Core\UseCase\Category\ListCategoryUseCase;
+use Core\UseCase\DTO\Category\CategoryInputDto;
+use Core\UseCase\DTO\Category\CategoryOutputDto;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+
+class ListCategoryUseCaseUnitTest extends TestCase {
+
+  public function testFindById() {
+    $id = (string) Uuid::uuid4()->toString();
+    $this->mockEntity = Mockery::mock(Category::class, [
+      $id,
+      'test category',
+    ]);
+
+    $this->mockRepository = Mockery::mock(CategoryRepositoryInterface::class);
+    $this->mockRepository->shouldReceive('findById')
+      ->with($id)
+      ->andReturn($this->mockEntity);
+
+    $this->mockInputDto = Mockery::mock(CategoryInputDto::class, [
+      $id,
+    ]);
+
+    $useCase = new ListCategoryUseCase($this->mockRepository);
+    $responseUseCase = $useCase->execute($this->mockInputDto);
+    $this->assertInstanceOf(CategoryOutputDto::class, $responseUseCase);
+    $this->assertEquals($id, $responseUseCase->id);
+    $this->assertEquals("test category", $responseUseCase->name);
+
+    /**
+     * Spies
+     */
+    $this->spy = Mockery::spy(CategoryRepositoryInterface::class);
+    $this->spy->shouldReceive('findById')
+      ->with($id)
+      ->andReturn($this->mockEntity);
+    $useCase = new ListCategoryUseCase($this->spy);
+    $responseUseCase = $useCase->execute($this->mockInputDto);
+    $this->spy->shouldHaveReceived('findById');
+  }
+
+  protected function tearDown(): void {
+    Mockery::close();
+    parent::tearDown();
+  }
+}
