e11d3706318592993ef19318a765e84bab271cd3
Danrley Lima <danrleylac@gmail.com>
2024-02-02 19:44:53 -0300

    refactor: update ListCategoriesUseCase to include pagination properties in output

--- src/Core/UseCase/Category/ListCategoriesUseCase.php
+++ src/Core/UseCase/Category/ListCategoriesUseCase.php
@@ -19,9 +19,35 @@ class ListCategoriesUseCase {
       totalPage: $input->totalPage
     );
 
+    // return new ListCategoriesOutputDto(
+    //   items: array_map(function ($data) {
+    //     var_dump($data);
+    //     return [
+    //       'id' => $data->id,
+    //       'name' => $data->name,
+    //       'description' => $data->description,
+    //       'is_active' => $data->is_active,
+    //       'created_at' => $data->created_at,
+    //       'updated_at' => $data->updated_at,
+    //       'deleted_at' => $data->deleted_at,
+    //     ];
+    //   }, $categories->items() ?? []),
+    //   total: $categories->total(),
+    //   last_page: $categories->lastPage(),
+    //   first_page: $categories->firstPage(),
+    //   per_page: $categories->perPage(),
+    //   to: $categories->to(),
+    //   from: $categories->from(),
+    // );
+
     return new ListCategoriesOutputDto(
       items: $categories->items(),
       total: $categories->total(),
+      last_page: $categories->lastPage(),
+      first_page: $categories->firstPage(),
+      per_page: $categories->perPage(),
+      to: $categories->to(),
+      from: $categories->from(),
     );
   }
 }
