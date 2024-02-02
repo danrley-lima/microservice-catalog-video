39f5f5fcb56bcf873031af278f0146b665f20728
Danrley Lima <danrleylac@gmail.com>
2024-02-02 13:42:25 -0300

    feat: add PaginationInterface for paginate in CategoryRepositoryInterface

--- src/Core/Domain/Repository/CategoryRepositoryInterface.php
+++ src/Core/Domain/Repository/CategoryRepositoryInterface.php
@@ -8,7 +8,7 @@ interface CategoryRepositoryInterface {
   public function insert(Category $category): Category;
   public function findById(int $id): ?Category;
   public function findAll(string $filter = "", $order = "DESC"): array;
-  public function paginate(string $filter = "", $order = "DESC", int $page = 1, int $totalPage = 15): array;
+  public function paginate(string $filter = "", $order = "DESC", int $page = 1, int $totalPage = 15): PaginationInterface;
   public function update(Category $category): Category;
   public function delete(string $id): bool;
   public function toCategory(object $data): Category;
