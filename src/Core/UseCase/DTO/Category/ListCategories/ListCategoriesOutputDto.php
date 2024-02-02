3b5b900d9eaab29ebfc00c1d502ab7c2a72b57a7
Danrley Lima <danrleylac@gmail.com>
2024-02-02 19:41:13 -0300

    feat: add pagination properties to ListCategoriesOutputDto

--- src/Core/UseCase/DTO/Category/ListCategories/ListCategoriesOutputDto.php
+++ src/Core/UseCase/DTO/Category/ListCategories/ListCategoriesOutputDto.php
@@ -6,6 +6,11 @@ class ListCategoriesOutputDto {
   public function __construct(
     public array $items,
     public int $total,
+    public int $last_page,
+    public int $first_page,
+    public int $per_page,
+    public int $to,
+    public int $from,
   ) {
   }
 }
